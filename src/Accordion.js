5d6dc98b6d82715ffb4dbe7bd24374e3882ef144
ovishekdas <ovishekdas@gmail.com>
2022-03-29 16:48:26 +0530

    new

--- src/Accordion.js
+++ src/Accordion.js
@@ -4,6 +4,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Label } from '@mui/icons-material';
 
 const AccordionComp = () => {
 
@@ -11,27 +12,43 @@ const AccordionComp = () => {
     //dmId: '1',
     demographic: [
       {
-        fieldId: "999",
-        fieldName: "my fname",
-        mockData: "hhhh",
+        fieldId: "First Name",
+        fieldName: "fName",
+        mockData: "f name",
         emma_field_name: "emma fieldname",
       },
       {
-        fieldId: "",
-        fieldName: "",
-        mockData: "",
+        fieldId: "Last Name",
+        fieldName: "lName",
+        mockData: "l name",
         emma_field_name: "",
       }
     ],
     provider: [
       {
-        fieldId: "",
-        fieldName: "",
-        mockData: "",
+        fieldId: "Provider Name",
+        fieldName: "pName",
+        mockData: "p name",
+        emma_field_name: "",
+      },
+      {
+        fieldId: "Another Provider Name",
+        fieldName: "APName",
+        mockData: "l name",
         emma_field_name: "",
       }
     ],
   })
+
+  const onChangeText = (keyHeader, i, e)=>{
+    //console.log(e.target.value)
+    //console.log(i)
+    simulationData[keyHeader][i].mockData = e.target.value
+    setSimulationData({
+      ...simulationData,
+      //[keyHeader] : [...simulationData[keyHeader], {...simulationData[keyHeader][i]}],
+    })
+  }
 
   return (
     <div>
@@ -52,7 +69,14 @@ const AccordionComp = () => {
                    
                     return (
                       <Typography key={i}>
-                        {JSON.stringify(row)}
+                       
+                          {row.fieldId} : 
+                       
+                        <input
+                            type="text"
+                            value={row.mockData}
+                            onChange={(e)=>{onChangeText(keyHeader, i, e)}}
+                        />
                       </Typography>
                     )
                   })
